8d7561e4b5f8d86a00fcdc28e9918250e2e2f506
lokman-hosen <lokman@mcc.com.bd>
2021-02-19 00:14:57 +0600

    image added in baner section

--- src/components/home/branding.js
+++ src/components/home/branding.js
@@ -1,5 +1,9 @@
 import react from 'react'
 import { Link } from 'react-router-dom';
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+import img3 from '../assets/img3.png'
+
 
 function Branding() {
 
@@ -7,18 +11,21 @@ function Branding() {
         { 
             id: 1, 
             title: "Brand Design",
-            discription: "Lorem ipsum dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna."
+            discription: "Lorem ipsum dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna.",
+            image: img1
         },
 
         { 
             id: 2, 
             title: "Seo & Marketing",
-            discription: "Lorem ipsum dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna."
+            discription: "Lorem ipsum dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna.",
+            image: img2
         },
         { 
             id: 3, 
             title: "Creative Design",
-            discription: "Lorem ipsum dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna."
+            discription: "Lorem ipsum dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna.",
+            image: img3
         },
        
       ];
@@ -32,14 +39,13 @@ function Branding() {
                 {data.map((item) => (
                     <div className="col-lg-4 col-md-6" key={item.id}>
                     <div className="service-item mb-4 mb-lg-0 text-center px-4">
-                        <i className="ti-ruler-pencil text-color icon-md"></i>
+                        <img src={item.image}/>
                         <h4 className="my-4 fw-bold">{item.title}</h4>
                         <p>{item.discription}</p>
                     </div>
                 </div>
                 ))}
                     
-                   
                 </div>
             </div>
         </section>
